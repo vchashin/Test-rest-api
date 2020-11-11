2df649726be4777292e5b9da9b8c5ba9431a20c3
Vladimir Kanygin <vladkant09@gmail.com>
2020-11-12 01:18:34 +0600

    Add user API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users/user.model";
+import {UsersModule} from "./users/users.module";
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
